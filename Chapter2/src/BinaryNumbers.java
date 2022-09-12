dd5c74dbc09c0e368e4df5dac0fb7f25a9106f62
1100037074 <1100037074@L815WD220005.edu.cbe.ab.ca>
2022-09-12 11:58:25 -0600

    11:58AM

--- Chapter2/src/BinaryNumbers.java
+++ Chapter2/src/BinaryNumbers.java
@@ -4,7 +4,35 @@ public class BinaryNumbers
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		System.out.println
+		("Decimal \t Binary\r\n"
+		+"      1               1\r\n"	
+		+"      2              10\r\n"
+		+"      3              11\r\n"
+		+"      4             100\r\n"
+		+"      5             101\r\n"
+		+"      6             110\r\n"
+		+"      7             111\r\n"
+		+"      8            1000\r\n"
+		+"      9            1001\r\n"
+		+"     10            1010\r\n"
+		+"     11            1011\r\n"
+		+"     12               1\r\n"
+		+"     13               1\r\n"
+		+"     14               1\r\n"
+		+"     15               1\r\n"
+		+"     16               1\r\n"
+		+"     17               1\r\n"
+		+"     18               1\r\n"
+		+"     19               1\r\n"
+		+"     20               1\r\n"
+	
+				
+				
+				
+				
+				);
+
 
 	}
 
