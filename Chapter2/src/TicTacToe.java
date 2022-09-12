e495aabe2c9e2d1498637336e8765faf2c2feab8
1100037074 <1100037074@L815WD220005.edu.cbe.ab.ca>
2022-09-12 10:53:01 -0600

    10:53AM

--- Chapter2/src/TicTacToe.java
+++ Chapter2/src/TicTacToe.java
@@ -1,11 +1,45 @@
-
+/*
+Program: ${TicTacToe}.java          Date: ${September 12, 2022}
+Author: Isaac Daodu
+School: CHHS
+Course: Computer Science 20
+ 
+*/
 public class TicTacToe 
 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Isaac");
+		System.out.println
+		("     |        |\r\n"
+		+ "     |        |\r\n"
+		+ "     |        |\r\n"		
+		+"--------------------- \r\n"		
+	
+		+ "     |        |\r\n"
+		+ "     |    x   |\r\n"
+		+ "     |        |\r\n"
+		+"--------------------- \r\n"
+		+ "     |        |\r\n"
+		+ "     |        |\r\n"
+		+ "     |        |\r\n"
+				);
+		
 
 	}
 
 }
+/* Screen Dump
+     |        |
+     |        |
+     |        |
+--------------------- 
+     |        |
+     |    x   |
+     |        |
+--------------------- 
+     |        |
+     |        |
+     |        |
+
+	 */
