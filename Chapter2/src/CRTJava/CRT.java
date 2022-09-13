68d4348ff2418dfacaf74a0ea8cc6ad2f1c17e60
1100037074 <1100037074@L815WD220005.edu.cbe.ab.ca>
2022-09-13 10:21:56 -0600

    10:21AM

---
+++ Chapter2/src/CRTJava/CRT.java
@@ -0,0 +1,38 @@
+/*
+Program: ${CRT}.java          Date: ${September 13, 2022}
+Author: Isaac Daodu
+School: CHHS
+Course: Computer Science 20
+ 
+*/
+package CRTJava;
+
+public class CRT 
+{
+
+	public static void main(String[] args) 
+	{
+		/*
+		 
+		 1. Average.java
+		 	
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 */
+	
+
+	}
+
+}
